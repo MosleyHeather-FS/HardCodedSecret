82ca888964d41830cde4cd9bb0af666f3fd8ed1d
Heather Mosley <hmosley@student.fullsail.edu>
2023-04-22 23:09:07 -0500

    added hard coded secret

---
+++ app.js
@@ -0,0 +1,18 @@
+require("dotenv").config("./.env");
+const answer = "A spelling bee";
+function Joke() {
+    return `What's the smartest insect? ${answer}`
+}
+const http = require("http")
+const hostname = "127.0.0.1"
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end(Joke())
+})
+
+server.listen(port, hostname, () => {
+
+})
